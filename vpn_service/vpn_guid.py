0dcae1c5783a745c374bcfa0c04edfbe429be4f6
Crimson-Amir <ytchanneltinos@gmail.com>
2024-10-05 18:41:52 +0330

    add quide and support

--- vpn_service/vpn_guid.py
+++ vpn_service/vpn_guid.py
@@ -28,22 +28,22 @@ async def vpn_guide(update, context):
     platform = query.data.replace('vpn_guid__', '')
 
     if platform == 'android':
-        text = f"{await ft_instance.find_text('android_guid')}"
+        text = f"{await ft_instance.find_text('android_guide')}"
         text += '\n\n<a href="https://play.google.com/store/apps/details?id=com.v2ray.ang&hl=en&pli=1">Google Play</a>'
         text += '\n<a href="https://github.com/2dust/v2rayng/releases">GitHub</a>'
 
     elif platform == 'ios':
-        text = f"{await ft_instance.find_text('ios_guid')}"
+        text = f"{await ft_instance.find_text('ios_guide')}"
         text += '\n\n<a href="https://apps.apple.com/us/app/streisand/id6450534064">Streisand</a>'
         text += '\n<a href="https://apps.apple.com/us/app/v2box-v2ray-client/id6446814690">V2Box</a>'
 
     elif platform == 'windows':
-        text = f"{await ft_instance.find_text('windows_guid')}"
+        text = f"{await ft_instance.find_text('windows_guide')}"
         text += '\n\n<a href="https://github.com/2dust/v2rayN/releases/download/6.23/v2rayN-With-Core.zip">v2rayN</a>'
 
     else:
         text = f"{await ft_instance.find_text('error_message')}"
 
-    keyboard = [[InlineKeyboardButton(await ft_instance.find_keyboard('back_button'), callback_data='guide_menu')]]
+    keyboard = [[InlineKeyboardButton(await ft_instance.find_keyboard('back_button'), callback_data='vpn_guide_menu')]]
 
     await query.edit_message_text(text=text, parse_mode='html', reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True)
