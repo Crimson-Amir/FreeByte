a33e124c2fd6b6a9641f3633c221ae0c359ca352
Crimson-Amir <ytchanneltinos@gmail.com>
2024-10-05 03:57:45 +0330

    add setting section

--- ddd.py
+++
@@ -1,8 +0,0 @@
-from database_sqlalchemy import SessionLocal
-import models_sqlalchemy as model
-
-with SessionLocal() as session:
-    all_user = session.query(model.UserDetail).all()
-    for user in all_user:
-        config = model.UserConfig(chat_id=user.chat_id)
-        session.add(config)
--- setting_menu.py
+++ setting_menu.py
@@ -11,7 +11,7 @@ async def setting_menu(update, context):
     text = await ft_instance.find_text('select_section')
 
     keyboard = [
-        [InlineKeyboardButton(await ft_instance.find_keyboard('vpn_setting_lable'), callback_data='vpn_main_menu')],
+        [InlineKeyboardButton(await ft_instance.find_keyboard('vpn_setting_lable'), callback_data='vpn_setting_menu')],
         [InlineKeyboardButton(await ft_instance.find_keyboard('back_button'), callback_data='start')]
     ]
 
