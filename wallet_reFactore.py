21224429e61caa5bea1fce50419b19abbba678d6
Crimson-Amir <ytchanneltinos@gmail.com>
2024-10-05 19:33:09 +0330

    update wallet section

--- wallet_reFactore.py
+++ wallet_reFactore.py
@@ -75,7 +75,7 @@ async def financial_transactions_wallet(update, context):
 
                     }
                     lasts_report += f"\n\n{await ft_instance.find_text('receive_money') if report.operation in ['recive', 'refund'] else await ft_instance.find_text('spend_money')} {report.amount:,} {await ft_instance.find_text('irt')} | "
-                    lasts_report += f"{payment_status.get(report.register_date.replace(microsecond=0))}"
+                    lasts_report += f"{report.register_date.replace(microsecond=0)}"
                     lasts_report += f"\n{payment_status.get(report.payment_status, '')}"
                     lasts_report += f"\n{payment_action.get(report.action, '')} {report.id_holder}"
             else:
