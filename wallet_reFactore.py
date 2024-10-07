16e986f7a5d4227e7354a221eb35493ab2e42d0f
Crimson-Amir <ytchanneltinos@gmail.com>
2024-10-07 20:16:57 +0330

    add usage report

--- wallet_reFactore.py
+++ wallet_reFactore.py
@@ -189,10 +189,10 @@ async def create_invoice(update, context):
             keyboard = [list(filter(None, row)) for row in keyboard]
 
             text = (f"<b>{await ft_instance.find_text('invoice_title')}"
-                    f"\n\n{await ft_instance.find_text('price')} {amount:,} {await ft_instance.find_text('irt')}"
+                    f"\n\n{await ft_instance.find_text('price')} {amount:,} {await ft_instance.find_text('irt')}</b>"
                     f"\n\n{await ft_instance.find_text('invoice_extra_data')}\n{invoice_extra_data}"
-                    f"\n\n{await ft_instance.find_text('wallet_credit_label')} {finacial_report.owner.wallet:,}"
-                    f"\n{await ft_instance.find_text('payment_option_title')}</b>")
+                    f"\n\n<b>{await ft_instance.find_text('wallet_credit_label')} {finacial_report.owner.wallet:,}</b>"
+                    f"\n{await ft_instance.find_text('payment_option_title')}")
 
             await query.edit_message_text(text=text, parse_mode='html', reply_markup=InlineKeyboardMarkup(keyboard))
 
