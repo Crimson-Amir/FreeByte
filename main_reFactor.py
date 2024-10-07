15d74f8fec7ab09ffebd1840d814a73626c0fc59
Crimson-Amir <ytchanneltinos@gmail.com>
2024-10-07 14:05:22 +0330

    add usage report

--- main_reFactor.py
+++ main_reFactor.py
@@ -82,7 +82,7 @@ if __name__ == '__main__':
     application.add_handler(CallbackQueryHandler(buy_and_upgrade_service.recive_test_service, pattern='vpn_recive_test__(.*)'))
     application.add_handler(CallbackQueryHandler(statistics.report_section, pattern='statistics_(.*)'))
     application.add_handler(my_service_detail.change_ownership_conversation)
-    application.job_queue.run_repeating(statistics.statistics_timer, interval=60, first=0)
+    application.job_queue.run_repeating(statistics.statistics_timer, interval=180 * 60, first=0)
     application.job_queue.run_repeating(panel_api.marzban_api.refresh_connection_schedule, interval=720 * 60, first=0)
 
     # Admin
