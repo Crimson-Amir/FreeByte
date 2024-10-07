32c52feea1d94e4e996e68c9a1f48afedbe44918
Crimson-Amir <ytchanneltinos@gmail.com>
2024-10-08 02:01:34 +0330

    add usage report

--- admin/admin_page.py
+++ admin/admin_page.py
@@ -125,7 +125,7 @@ async def add_credit_for_user(update, context):
 
                 finacial_report = crud.create_financial_report(
                     session, 'recive',
-                    chat_id=user_detail.id,
+                    chat_id=user_chat_id,
                     amount=int(amount),
                     action='increase_balance_by_admin',
                     service_id=None,
