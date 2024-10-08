6e32ad211d2b22ca8e84f5b3fef4dcd248dfadeb
Crimson-Amir <ytchanneltinos@gmail.com>
2024-10-09 01:37:56 +0330

    add admin panel

--- admin/admin_ticket.py
+++ admin/admin_ticket.py
@@ -29,7 +29,7 @@ async def reply_ticket(update, context):
     context.user_data[f'ticket_user_id'] = user_id
     keyboard = [[InlineKeyboardButton("Cancel", callback_data='cancel_reply_ticket_conversation')]]
     text = 'send message (photo or text):'
-    await context.bot.send_message(text=text, chat_id=user_detail.id, parse_mode='html', message_thread_id=setting.ticket_thread_id if context.user_data[f'ticket_private'] else None, reply_markup=InlineKeyboardMarkup(keyboard))
+    await context.bot.send_message(text=text, chat_id=user_detail.id, parse_mode='html', message_thread_id=setting.ticket_thread_id if not context.user_data[f'ticket_private'] else None, reply_markup=InlineKeyboardMarkup(keyboard))
     return REPLY_TICKET
 
 async def assurance(update, context):
