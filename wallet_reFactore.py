f1c9ebba7c6fcc954a001186ef77fb274178fb8a
Crimson-Amir <ytchanneltinos@gmail.com>
2024-10-13 23:11:49 +0330

    add admin panel

--- wallet_reFactore.py
+++ wallet_reFactore.py
@@ -204,7 +204,7 @@ async def create_invoice(update, context):
                     f"\n\n<b>{await ft_instance.find_text('wallet_credit_label')} {finacial_report.owner.wallet:,} {await ft_instance.find_text('irt')}</b>"
                     f"\n\n{await ft_instance.find_text('invoice_extra_data')}\n{invoice_extra_data}"
                     f"\n\n<b>{price_text} {amount:,} {await ft_instance.find_text('irt')}</b>"
-                    f"\n{await ft_instance.find_text('payment_option_title')}")
+                    f"\n\n{await ft_instance.find_text('payment_option_title')}")
 
             await query.edit_message_text(text=text, parse_mode='html', reply_markup=InlineKeyboardMarkup(keyboard))
 
