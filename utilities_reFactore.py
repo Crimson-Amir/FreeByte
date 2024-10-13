14f51ef109bbb5d1461413f82e0e1e19493a9490
Crimson-Amir <ytchanneltinos@gmail.com>
2024-10-13 22:24:53 +0330

    add admin panel

--- utilities_reFactore.py
+++ utilities_reFactore.py
@@ -64,7 +64,7 @@ async def start(update, context, in_new_message=False, raise_error=False):
         await context.bot.send_message(chat_id=user_detail.id, text='<b>Sorry, somthing went wrong!</b>', parse_mode='html')
 
 
-async def find_user(session, user_id, context=None):
+async def find_user(session, user_id, context):
     user_database_id = context.user_data.get('user_database_id')
     if not user_database_id:
         get_user_user_database_id_from_db = crud.get_user(session, user_id)
