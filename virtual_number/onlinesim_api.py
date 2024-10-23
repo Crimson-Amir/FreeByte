4a9019cfa54d2bfbd668dc4d1f1f43609a4a0bc1
Crimson-Amir <ytchanneltinos@gmail.com>
2024-10-23 04:09:20 +0330

    add virtual number section

---
+++ virtual_number/onlinesim_api.py
@@ -0,0 +1,82 @@
+import os, sys, requests, pprint
+from urllib.parse import urlparse, parse_qsl, urlunparse, urlencode
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import setting
+
+class OnlineSim:
+    def __init__(self, api_key):
+        self._api_key = api_key
+        self.session = requests.session()
+
+    def make_request(self, method, endpoint, **kwargs):
+        """Make an HTTP request and return JSON response."""
+        url = f'https://onlinesim.io/api/{endpoint}'
+        response = self.session.request(method, url, **kwargs)
+        response.raise_for_status()
+        return response.json()
+
+    def init_requests(self, endpoint: str, **queris):
+        queris.setdefault('lang', 'en')
+        queris.update({'apikey': self._api_key})
+        parsed_url = urlparse(endpoint)
+        url_queris = dict(parse_qsl(parsed_url.query))
+        url_queris.update(queris)
+        new_queris = urlencode(url_queris)
+        new_url = urlunparse(parsed_url._replace(query=new_queris))
+        return new_url
+
+    def get_tariffs(self, **params):
+        endpoint = self.init_requests(f"getTariffs.php", **params)
+        return self.make_request('get', endpoint)
+
+    def get_number(self, service_name, country_code, number=True, **params):
+        endpoint = self.init_requests(
+            "getNum.php", service=service_name, country=country_code, number=number, **params
+        )
+        return self.make_request('get', endpoint)
+
+    def get_state(self, **params):
+        endpoint = self.init_requests(f"getState.php", **params)
+        return self.make_request('get', endpoint)
+
+    def set_operation_ok(self, **params):
+        endpoint = self.init_requests(f"setOperationOk.php", **params)
+        return self.make_request('get', endpoint)
+
+    def get_free_list(self, **params):
+        endpoint = self.init_requests(f"getFreeList", **params)
+        return self.make_request('get', endpoint)
+
+
+    def get_rent_tariffs(self, **params):
+        endpoint = self.init_requests(f"tariffsRent.php", **params)
+        return self.make_request('get', endpoint)
+
+    def get_rent_number(self, country_code, days, **params):
+        endpoint = self.init_requests(
+            "getRentNum.php", country=country_code, days=days, **params
+        )
+        return self.make_request('get', endpoint)
+
+    def get_rent_state(self, tzid, **params):
+        endpoint = self.init_requests(
+            "getRentState.php", tzid=tzid, **params
+        )
+        return self.make_request('get', endpoint)
+
+    def extend_rent_state(self, tzid, days, **params):
+        endpoint = self.init_requests(
+            "extendRentState.php", tzid=tzid, days=days, **params
+        )
+        return self.make_request('get', endpoint)
+
+    def close_rent_number(self, tzid, **params):
+        endpoint = self.init_requests(
+            "closeRentNum.php", tzid=tzid, **params
+        )
+        return self.make_request('get', endpoint)
+
+a = OnlineSim(setting.onlinesim_api)
+# b = a.get_number('facebook', 7)
+b = a.get_free_list(country=7)
+pprint.pprint(b)
