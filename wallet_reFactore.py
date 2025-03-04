74c3380ed5b1b24946f07f2c13b4f51ab425887a
CrimsonAmir <ytchanneltinos@gmail.com>
2025-03-04 14:20:08 +0330

    fix some issues

--- wallet_reFactore.py
+++ wallet_reFactore.py
@@ -209,11 +209,14 @@ async def create_invoice(update, context):
                 if get_user.user_level > 1 and pay_by_wallet_satatus else await ft_instance.find_text('price')
             price_text = price_text.format(vpn_utilities.DiscountPerLevel.descount.get(get_user.user_level, 1))
 
-            text = (f"<b>{await ft_instance.find_text('invoice_title')}</b>"
-                    f"\n\n<b>{await ft_instance.find_text('wallet_credit_label')} {finacial_report.owner.wallet:,} {await ft_instance.find_text('irt')}</b>"
-                    f"\n\n{await ft_instance.find_text('invoice_extra_data')}\n{invoice_extra_data}"
-                    f"\n\n<b>{price_text} {amount:,} {await ft_instance.find_text('irt')}</b>"
-                    f"\n\n{await ft_instance.find_text('payment_option_title')}")
+            text = f"<b>{await ft_instance.find_text('invoice_title')}</b>"
+
+            if finacial_report.owner.wallet:
+                text += f"\n\n<b>{await ft_instance.find_text('wallet_credit_label')} {finacial_report.owner.wallet:,} {await ft_instance.find_text('irt')}</b>"
+
+            text += (f"\n\n{await ft_instance.find_text('invoice_extra_data')}\n{invoice_extra_data}"
+                     f"\n\n<b>{price_text} {amount:,} {await ft_instance.find_text('irt')}</b>"
+                     f"\n\n{await ft_instance.find_text('payment_option_title')}")
 
             await query.edit_message_text(text=text, parse_mode='html', reply_markup=InlineKeyboardMarkup(keyboard))
 
